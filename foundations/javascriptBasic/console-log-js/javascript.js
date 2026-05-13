421437388c4327855f4c6b0916330db8a1ed6cb3
Amin Nur Muis <nusantaralive@outlook.com>
2026-05-13 18:20:11 +0700

    new javascript practices

---
+++ foundations/javascriptBasic/console-log-js/javascript.js
@@ -0,0 +1,30 @@
+   // Your JavaScript goes here!
+    console.log("Hello, World!")
+
+    let firstName = "Amin";
+    let lastName = "Muis";
+
+    console.log(firstName);
+    console.log(lastName);
+    
+    // let can reassign
+    let age = 20;
+    console.log(age);
+    age = 21;
+    console.log(age);
+
+    // const can't reassign
+    const pi = 3.14;
+    console.log(pi);
+    console.log(pi);
+
+    let add = 23+97;
+    console.log("23+97= ${add}");
+    console.log(add)
+
+    const string = "hello, I'm amin";
+    const badString = string;
+    console.log(string);
+
+    const string1 = `good morning, ${string}`;
+    console.log(string1)
