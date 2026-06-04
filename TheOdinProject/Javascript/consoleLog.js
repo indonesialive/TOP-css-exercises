d3ac6ca53cf24afb840bbece6b8d531b73a436b2
Amin Nur Muis <nusantaralive@outlook.com>
2026-06-04 20:29:59 +0700

    several updates

---
+++ TheOdinProject/Javascript/consoleLog.js
@@ -0,0 +1,18 @@
+let firstname = "Amin";
+const lastname = "Muis";
+
+console.log(firstname + " " + lastname);
+console.log((1+2)*4);
+console.log(1+2+1+2+1+5)
+
+const max=57;
+const actual=max-13;
+const percentage=actual/max;
+
+console.log(percentage);
+
+
+let a = 1;
+
+for (a=1;a<10;a++)
+{console.log(a);};
